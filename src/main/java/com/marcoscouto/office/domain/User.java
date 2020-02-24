62862f7ade931dd0409f910a7d3fa828cf527b47
Marcos Couto <marcos_otuoc@hotmail.com>
2020-02-24 17:58:05 -0300

    Entity and Repository

---
+++ src/main/java/com/marcoscouto/office/domain/User.java
@@ -0,0 +1,71 @@
+package com.marcoscouto.office.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+
+    private Long id;
+    private String name;
+    private Integer age;
+    private String office;
+
+    public User() {
+    }
+
+    public User(Long id, String name, Integer age, String office) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.office = office;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getOffice() {
+        return office;
+    }
+
+    public void setOffice(String office) {
+        this.office = office;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
